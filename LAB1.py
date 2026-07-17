a856df351d68c13a7e80977f1fe890ff319f1e5a
akshaya <25cseaiml058.guntamukkalaakshaya@giet.edu>
2026-07-17 09:19:40 +0530

    Updated LAB1.py

--- LAB1.py
+++ LAB1.py
@@ -1,1 +1,24 @@
-print("heloo")
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+visited = []
+queue = []
+start = 'A'
+
+visited.append(start)
+queue.append(start)
+
+while queue:
+    current = queue.pop(0)
+    print(current, end=" ")
+
+    for neighbour in graph[current]:
+        if neighbour not in visited:
+            visited.append(neighbour)
+            queue.append(neighbour)
